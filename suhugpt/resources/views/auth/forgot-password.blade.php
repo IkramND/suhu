f5236cc9daae3fae1d66776329b573c9ff75f4af
NextTI.Devo <muhammad.devo@next-ti.co.id>
2025-03-04 09:30:40 +0700

    move to public folder, laravel compilement

--- suhugpt/resources/views/auth/forgot-password.blade.php
+++ suhugpt/resources/views/auth/forgot-password.blade.php
@@ -5,8 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lupa Password</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script type="text/javascript" src="../resources/js/script.js"></script>
-    <script>
+    <script type="text/javascript" src="{{ asset('js/script.js') }}">
 
         let globalVarOTP = '';
 
